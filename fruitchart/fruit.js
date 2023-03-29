e44c75ea68cf7135c54531a078263ca4e93dea8d
Kev1nC <98926400+Kev1nC@users.noreply.github.com>
2023-03-29 15:23:18 -0400

    Added fruit lab

---
+++ fruitchart/fruit.js
@@ -0,0 +1,30 @@
+'use strict';
+
+// An array of fruit objects
+const fruit = [
+    {name:"Apple", quantity: 20, color: "red"},
+    {name:"Orange", quantity: 10, color: "orange"},
+    {name:"Banana", quantity: 15, color: "yellow"},
+    {name:"Kiwi", quantity: 5, color: "green"},
+    {name:"Blueberry", quantity: 5, color:"blue"},
+    {name:"Grapes", quantity: 10, color:"purple"}
+];
+
+window.onload = function() {
+    const canvas = document.getElementById('fruit-chart');
+    const ctx = canvas.getContext("2d");
+
+    let maxBarHeight = 160;
+    let start_y = 0;
+    let quantityCalc = 25;
+
+    for (let i = 0; i < fruit.length; i++) {
+        let calcWidth = fruit[i].quantity / quantityCalc * canvas.width;
+        ctx.fillStyle = fruit[i].color;
+        ctx.fillRect(0, start_y, calcWidth, maxBarHeight);
+        ctx.fillStyle = "black";
+        ctx.font = "20px Arial";
+        ctx.fillText(fruit[i].name, 10, start_y + maxBarHeight / 2);
+        start_y += maxBarHeight + 20;
+    }
+}
